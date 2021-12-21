de778797e376354eb460bac3d0435a4c3331b702
Andre Camargo <andre.camargo500@gmail.com>
2021-12-21 01:24:50 -0300

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from 'react-icons/fi';
 
 interface Task {
   id: number;
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+   
+    if(newTaskTitle ?? newTaskTitle > 0){
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('')
+    }
+
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const statusComplete = tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete;
+      }
+      return task
+    })
+    setTasks(statusComplete)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+
+    const statusDel = tasks.filter(task => task.id !== id);
+    setTasks(statusDel);
   }
 
   return (
